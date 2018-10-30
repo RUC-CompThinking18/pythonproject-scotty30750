49d18184eaae02a8396e1157032aaa1673ca5611
scotty30750 <43013351+scotty30750@users.noreply.github.com>
2018-10-30 16:40:52 -0400

    Added openfile function, readlines

--- oceanprojectcode.py
+++ oceanprojectcode.py
@@ -1,29 +1,15 @@
-class ocean():
-    def ocean_quiz(question, true_false, fact):
-        print question
-        if question is == True
-        print matching fact
-        if question is == False
-        print matching fact
-        Add up total score
-        print total score
-Fact 1: Plastic is the most common element that is found in the ocean. It is harmful for the environment as it does not get break down easily and is often considered as food by marine animals.
-Fact 2: The biggest source of pollution in the ocean is directly from land based sources, such as oil, dirt, septic tanks, farms, ranches, motor vehicles, among larger sources. Thousands of tons of waste and trash are dumped into the ocean on a daily basis.
-Fact 3: Over one million seabirds are killed by ocean pollution each year. Three hundred thousand dolphins and porpoises die each year as a result of becoming entangled in discarded fishing nets, among other items. One hundred thousand sea mammals are killed in the ocean by pollution each year.
-Fact 4: Even though much the trash and waste dumped into the ocean is released hundreds of miles away from land, it still washes up on beaches and coastal areas, and affects everything in between. Every marine animal is affected by man-made chemicals released in the water.
-Fact 5: There is an island of garbage twice the size of Texas inside the Pacific Ocean: the North Pacific Gyre off the coast of California is the largest oceanic garbage site in the entire world. There, the number of floating plastic pieces outnumbers total marine life six to one in the immediate vicinity.
-Fact 6: Oil is the fastest source of deterioration to the ocean, being far more harmful than trash and waste. However, only a small percentage of oil (around 12%) dumped in the ocean comes as a result of actual oil spills. Most oil causing harm in the ocean is a result of drainage from land. Oil spills suffocate marine life to death, and leads to behavioral changes and a breakdown in thermal insulation to those that do survive. It essentially changes the entire ecosystem of an affected area, such as a long coastline or deep ocean.
-Fact 7: Toxic metals can destroy the biochemistry, behavior, reproduction, and growth in marine life.
-Fact 8: Plastic debris can absorb toxic chemicals from ocean pollution, therefore poisoning whatever eats it. In fact, plastic pollution is one of the most serious threats to the ocean. Plastic does not degrade; instead, it breaks down into progressively smaller pieces, but never disappears. They then attract more debris. It poses a significant health threat to the various sea creatures, and to the entire marine ecosystem. Overall, plastic is the number one source of pollution in the ocean.
-Fact 9: Not all sources of contamination in the ocean come from just oil, trash and solid wastes. The dumping of radioactive waste from nuclear reactors, industrial race (such as heavy metals and acids), and drained sewage are also heavy contributors to pollution.
-Fact 10: Sewage leads to the decomposition of organic matter that in turn leads to a change in biodiversity. Even if the ocean’s ecosystem isn’t destroyed entirely, it is still changed drastically, and usually not for the better.
-Fact 11: Fertilizer runoff creates eutrophication that flourishes algal bloom (rapid increase or accumulation in the population of algae in aquatic systems) which depletes the oxygen content in the water that affects marine life.
-Fact 12: Small animals at the bottom of food chain absorb the chemicals as part of their food. These small animals are then eaten by larger animals that again increases the concentration of chemicals. Animals at the top of hierarchy of food chain have contamination levels millions times higher than the water in which they live.
-Fact 13: People get contaminated easily by eating contaminated seafood that can cause serious health problems, from cancer to damage to immune system.
-Fact 14: The garbage like plastic bottles, aluminium cans, shoes, packaging material – if not disposed correctly, can reach the sea and the same garbage can again reach the sea shore where it pollutes beaches and affects local tourism industry.
-Fact 15: Salty water of ocean has the capability to move pollutants from the ocean into coastal freshwater making wells and groundwater contaminated.
-Fact 16: Chemicals from industries and mines can also enter ocean through land based activities. They can seep through soil, water or land either during their manufacture, use or accidental leaks. From soil, water or land, they can enter into ocean currents and can travel longer distances.
-Fact 17: As 70% of the earth is covered with water, people actually assumed that all pollutants would be diluted and get disappeared. But in reality, they have not disappeared and their effects can be easily seen as they have entered the food chain.
-Fact 18: Until 1970’s, the chemicals and garbage was deliberately dumped into the oceans and became as common practice for disposing everything including pesticides and radioactive waste, assuming that it would get dissolved to safe levels.
-Fact 19: In several parts of the world including Gulf of Mexico and the Baltic Sea, Eutrophication has created enormous dead zones.
-Fact 20: Till today, in many parts of the world, sewage water is discharged in the ocean – untreated or under-treated. This can cause serious effect on marine and human life and can also lead to eutrophication.
+"""
+def ocean_quiz(question, true_false, fact):
+    print question
+    if question is == True
+    print matching fact
+    if question is == False
+    print matching fact
+    print total score
+"""
+def openfile():
+    show_file = open("qf.txt", "r")
+    return showfile.readlines()
+    for i in range (40):
+        print i
+openfile()
