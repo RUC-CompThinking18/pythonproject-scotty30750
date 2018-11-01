61c24effd202c02c98d2a1f365d4394cd57516f3
scotty30750 <43013351+scotty30750@users.noreply.github.com>
2018-11-01 15:33:27 -0400

    added meaningful comments to my code

--- oceanprojectcode.py
+++ oceanprojectcode.py
@@ -1,4 +1,11 @@
-"""
+def openfile():
+    show_file = open("qf.txt", "r")
+#this function opens up my text so that the facts and questions can be available
+    return showfile.readlines()
+    for i in range (40):
+#will spit out lines 0 to 40
+        print i
+openfile()
 def ocean_quiz(question, true_false, fact):
     print question
     if answer is == True
@@ -6,12 +13,6 @@ def ocean_quiz(question, true_false, fact):
         print matching fact
     if answer is == False
         print matching fact
-
-    print total score
-"""
-def openfile():
-    show_file = open("qf.txt", "r")
-    return showfile.readlines()
-    for i in range (40):
-        print i
-openfile()
+#fact will print after question is asked and add total score
+print total score
+reconfigure this to get this rawinput
